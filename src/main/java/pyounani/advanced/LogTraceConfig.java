594b62953f15dcfcb360dce288c56f4afa173fb5
pyounani <yndbsk9372@gmail.com>
2024-10-31 15:04:07 +0900

    [Feat] ThreadLocal 적용

--- src/main/java/pyounani/advanced/LogTraceConfig.java
+++ src/main/java/pyounani/advanced/LogTraceConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pyounani.advanced.trace.logtrace.FieldLogTrace;
 import pyounani.advanced.trace.logtrace.LogTrace;
+import pyounani.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        //return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
